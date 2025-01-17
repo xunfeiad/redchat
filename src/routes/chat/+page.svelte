c7bb5f9ac9a30281f2de9ce6afb27a5cf6e263c2
xunfei <xfhd520@126.com>
2025-01-18 02:21:43 +0800

    feat: add webrtc to support instant message

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -299,10 +299,6 @@
       });
       localVideo!.srcObject = localStream;
 
-      localPeerConnection = new RTCPeerConnection({
-        iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-      });
-
       // 添加本地流
       localStream.getTracks().forEach((track) => {
         localPeerConnection!.addTrack(track, localStream!);
